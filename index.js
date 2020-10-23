c30debe7960d6652eb64c34732b0ccc7b97af594
Shamel Lakin <christlife4u@gmail.com>
2020-10-22 20:40:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Your code here
+ let createEmployeeRecord = function(dbRow){
+    return { 
+		firstName: dbRow[0],
+        familyName: dbRow[1],
+        title: dbRow[2],
+        payPerHour: dbRow[3],
+        timeInEvents: [],
+        timeOutEvents: []
+    }
+        
+      
+}
+
+let createEmployeeRecords = function(dbRow){
+    let  newArr = []
+    for ( let i = 0; i < dbRow.length; i++ ){
+       
+        newArr.push(createEmployeeRecord(dbRow[i]))
+    }
+    return newArr
+}
+
+let createTimeInEvent = function(createEmployeeRecord, timeStamp){
+    return {
+        type: TimeIn
+        hour: 
+        date:
+    }
+
+}
